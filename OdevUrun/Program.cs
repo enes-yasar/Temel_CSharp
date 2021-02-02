fb23e9163d53e909c5977555a8d16fd191d58759
Enes <75998802+enes-yasar@users.noreply.github.com>
2021-02-02 18:03:10 +0300

    Add project files.

---
+++ OdevUrun/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace OdevUrun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] urunler = new string[] { "Elektronik", "Moda","Bahçe","Kitap" };
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun);
+            }
+            Console.WriteLine("----------------------");
+            string[] Elektronik = new string[] { "Bilgisayar", "Telefon", "Tablet", "Televizyon", "Yazıcı" };
+            foreach (var i in Elektronik)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("-----------------------");
+            string[] Moda = new string[]  {"Tişört","Kazak","Mont","Ayakkabı","Gömlek"};
+            foreach (var i in Moda)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("-----------------");
+            string[] Bahce = new string[] { "Mangal","Barbekü","Aydınlatma" };
+            foreach (var i in Bahce)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("-----------------");
+            string[] Kitap = new string[] {"Edebiyat","Dergi","Roman","Çocuk" };
+            foreach (var i in Kitap)
+            {
+                Console.WriteLine(i);
+            }
+                
+            
+        
+        
+        
+        
+        }   
+    }
+}
---
+++ Temel_CSharp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Temel_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string katogeriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOranı = 1.45;
+            bool sistemGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış oku");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış Oku");
+            }
+            else
+            {
+                Console.WriteLine(dolarDun == dolarBugun);
+            }
+
+            if (sistemGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kulanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giris yap butonu");
+            }
+
+            Console.WriteLine(katogeriEtiketi);
+        
+        }
+
+    }
+}
